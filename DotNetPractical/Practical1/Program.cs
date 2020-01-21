d414d6280e6c31f26fd6ad8e8531fd1b2c0072f0
paramraval <17.comp.param.ravall@gmail.com>
2020-01-21 13:54:47 +0530

    Practiacal1

--- DotNetPractical/Practical1/Program.cs
+++ DotNetPractical/Practical1/Program.cs
@@ -7,9 +7,99 @@ namespace Practical1
 {
     class Program
     {
+        static void pattern1()
+        {
+            for (int i=1; i<=5; i++)
+            {
+                for (int j=1; j<=i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+
+        static void pattern2()
+        {
+            for (int i=1; i<=5; i++)
+            {
+                for (int j=5; j>=i; j--)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+
+        static void pattern3()
+        {
+             for (int i=1; i<=5; i++)
+             {
+                for (int j=1; j<=5; j++)
+                {
+                    if (i <= j)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+
+        static void pattern4()
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = 1; j <= 5; j++)
+                {
+                    if (i < j)
+                    {
+                        Console.Write("#");
+                    }
+                    else
+                    {
+                        Console.Write("*");
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+
+        static void pattern5()
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = 1; j <= 5; j++)
+                {
+                    if ()
+                    {
+                        Console.Write("1");
+                    }
+                    else
+                    {
+                        Console.Write("0");
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("HII");
+            pattern1();
+            pattern2();
+            pattern3();
+            pattern4();
+            pattern5();
             Console.ReadLine();
         }
     }    
