c58ff5df6066ed06c6e924f6d36d1adfd858669b
Param Raval <52950939+paramraval@users.noreply.github.com>
2020-02-04 13:00:41 +0530

    Add files via upload

---
+++ practical2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace practical2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName, country;
+            do
+            {
+                Console.WriteLine("What's your name?");
+                firstName = Console.ReadLine();
+            }
+            while (!Regex.IsMatch(firstName, @"^[a-zA-Z]+$"));
+            do
+            {
+                Console.WriteLine("Where are you from?");
+                country = Console.ReadLine();
+            }
+            while (!Regex.IsMatch(country, @"^[a-zA-Z]+$"));
+
+            Console.WriteLine("Hello " + firstName + " from " + country);
+            Console.ReadLine();
+        }
+    }
+}
